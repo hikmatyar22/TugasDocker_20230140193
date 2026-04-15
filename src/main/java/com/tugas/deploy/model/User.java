a74e1d070b8d938c04afd8da6e2885cf5b849430
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2026-04-15 10:41:31 +0700

    Membuat class `User` sebagai model untuk menyimpan data user dan mahasiswa, seperti username, password, nama, NIM, dan jenis kelamin. Class ini juga menyediakan constructor serta getter dan setter untuk mengakses dan mengubah data.

---
+++ src/main/java/com/tugas/deploy/model/User.java
@@ -0,0 +1,35 @@
+package com.tugas.deploy.model;
+
+public class User {
+    private String username;
+    private String password;
+
+
+    private String nama;
+    private String nim;
+    private String jenisKelamin;
+
+
+    public User() {}
+
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getNama() { return nama; }
+    public void setNama(String nama) { this.nama = nama; }
+
+    public String getNim() { return nim; }
+    public void setNim(String nim) { this.nim = nim; }
+
+    public String getJenisKelamin() { return jenisKelamin; }
+    public void setJenisKelamin(String jenisKelamin) { this.jenisKelamin = jenisKelamin; }
+}
